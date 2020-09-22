a330eb5df92cbab909ce8c9c18443489b94139e4
rubenerangel <rubenrang@gmail.com>
2020-09-22 19:29:56 -0400

    Layout

--- app/controllers/Index.php
+++ app/controllers/Index.php
@@ -2,17 +2,49 @@
 
 class Index extends Controller
 {
+  private $userModel;
+
   public function __construct()
   {
-    // 
+    $this->userModel = $this->model('User');
   }
 
   public function index()
   {
+    $users = $this->userModel->users();
+
     $data = [
-      'title'     => 'Welcome to MVC',
+      'users'     => $users,
     ];
 
     $this->view('pages/index', $data);
+  }
+
+  public function add()
+  {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      $data = [
+        'name'  => trim( $_POST['name'] ),
+        'email'  => trim( $_POST['email'] ),
+        'phone'  => trim( $_POST['phone'] ),
+      ];
+
+      if ( $this->userModel->add( $data ) ) {
+        //redireccionar('paginas');
+      } else {
+        die('Something went Wrong...');
+      }
+    } else {
+      $data = [
+        'name'  => '',
+        'email'  => '',
+        'phone'  => '',
+      ];
+
+      $this->view('index/add', $data);
+    }
+    
+
+    
   }
 }
--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,5 +1,47 @@
 <?php require ROUTE_APP . '/views/includes/header.php'  ?>
 
-<h1><?php echo $data['title']; ?></h1>
+<main role="main">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center">
+        <h1>CRUD USERS</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12 mt-3">
+        <div class="d-flex flex-row-reverse mb-1">
+          <a class="btn btn-primary">Add</a>
+        </div>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php foreach($data['users'] as $user):  ?>
+              <tr>
+                <td><?php echo $user->id ?></td>
+                <td><?php echo $user->name ?></td>
+                <td><?php echo $user->email ?></td>
+                <td><?php echo $user->phone ?></td>
+                <td>
+                  <a href="<?php echo ROUTE_URL; ?>/index/edit/<?php echo $user->id; ?>">Edit</a>
+                  <a href="<?php echo ROUTE_URL; ?>/index/delete/<?php echo $user->id; ?>">Delete</a>
+                </td>
+              </tr>
+            <?php endforeach; ?>
+
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</main>
+
 
 <?php require ROUTE_APP . '/views/includes/footer.php'  ?>
